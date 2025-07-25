30e4d9cdf9b8d420602933d5d195a701a5cfc139
fanny_fy_chang <fanny_fy_chang@gss.com.tw>
2025-07-25 18:38:22 +0800

    修正

--- app.py
+++ app.py
@@ -20,5 +20,7 @@ for tab, result in zip(tabs, data):
         st.markdown(f"- 本國專利號：{result['target']}")
         st.markdown(f"- 美國專利號：{result['id']}")
         # bullet tag + expander
+        st.markdown("- LLM 回答：")
+
         with st.expander("點我展開/收合 LLM 回答", expanded=True):
             st.markdown(result['llm_result'])
